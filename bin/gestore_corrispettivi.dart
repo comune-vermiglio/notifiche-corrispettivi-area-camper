c4859bcda8c8ad77dcdc007658fcc7086d7b50c2
Pietro Panizza <pietropanizza88@gmail.com>
2025-08-11 18:19:05 +0200

    Initial dart template.

---
+++ bin/gestore_corrispettivi.dart
@@ -0,0 +1,5 @@
+import 'package:gestore_corrispettivi/gestore_corrispettivi.dart' as gestore_corrispettivi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gestore_corrispettivi.calculate()}!');
+}
---
+++ test/gestore_corrispettivi_test.dart
@@ -0,0 +1,8 @@
+import 'package:gestore_corrispettivi/gestore_corrispettivi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
